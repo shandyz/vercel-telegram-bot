a5c385a48e3d66287e1574a7527648cc19295a1e
shandyz <shandyztop@hotmail.com>
2024-08-04 21:05:34 +0200

    added description

--- src/text/description.ts
+++ src/text/description.ts
@@ -1,1 +1,8 @@
 import { Context } from 'telegraf';
+
+const description = () => async (ctx: Context) => {
+  const message = `Description`;
+  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
+};
+
+export { description };
